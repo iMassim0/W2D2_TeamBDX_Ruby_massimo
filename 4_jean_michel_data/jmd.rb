e7de6469aec813687d3d255d674ef013395e9307
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-10 22:05:27 +0200

    4.1 OK + fichiers txt

--- 4_jean_michel_data/jmd.rb
+++ 4_jean_michel_data/jmd.rb
@@ -1,3 +1,18 @@
-def jean_michel_data(corpus, dictionnary)
+def jean_michel_data(corpus, dictionary)
 
+  words = {}
+  corpus = corpus.downcase!
+
+  dictionary.each do |word|
+    if corpus.scan(/#{word}/).length == 0
+    else
+      words[word] = corpus.scan(/#{word}/).length
+    end
+  end
+print words
 end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+corpus = "below"
+
+jean_michel_data("Howdy partner, sit down! How's it going?", dictionary)
