399de1a33162dd5fdf6e1a0b9a16109dcd4cb42e
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-10 15:24:22 +0200

    "exo 01 terminé"

--- 2_nSA/nsa.rb
+++ 2_nSA/nsa.rb
@@ -1,5 +1,33 @@
 def chiffre_de_cesar(message, key)
 
-end
+  code = []
+  key = key.to_i
+  new_message = []
+  message = message.split(//)
 
-puts chiffre_de_cesar("ceci est un BRAVE testino", 6)
+  message.each do |x|
+    # -- On skip les espaces --
+    if x.ord == 32
+      code << x
+    # -- On traite les majuscules
+    elsif (65..90).include? x.ord
+        if x.ord + key > 90
+          code << x.ord + key - 25
+        else code << x.ord + key
+        end
+    # -- On traite les minuscules
+    elsif (97..122).include? x.ord
+        if x.ord + key > 122
+          code << x.ord + key - 25
+        else code << x.ord + key
+        end
+    else
+    end
+
+  end
+    # -- On transforme notre message codé en texte --
+  code.each do |char|
+    new_message << char.chr
+  end
+puts new_message.join("")
+end
